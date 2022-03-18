6a642863df7e4cf2d673042dec7841b7503b248f
cosmos2202 <100411382+cosmos2202@users.noreply.github.com>
2022-03-18 18:02:45 +0300

    Relase

--- main.py
+++ main.py
@@ -2,86 +2,125 @@ import os
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+from PyQt5.QtWinExtras import QtWin
+
+
+import multiprocessing
+quantity_threads = multiprocessing.cpu_count()
+
+user = open(r'resurs\user.txt', 'r+').read().split(',')
+
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(780, 540)
+        MainWindow.resize(780, 520)
         font = QtGui.QFont()
         font.setFamily("Times New Roman")
         MainWindow.setFont(font)
-        MainWindow.setStyleSheet("background-color: #5E338D;")
+        MainWindow.setStyleSheet("background: black;")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.button_ok = QtWidgets.QPushButton(self.centralwidget)
-        self.button_ok.setGeometry(QtCore.QRect(584, 342, 80, 30))
-        font = QtGui.QFont()
-        font.setFamily("Times New Roman")
-        font.setPointSize(14)
-        font.setBold(True)
-        font.setWeight(75)
-        self.button_ok.setFont(font)
-        self.button_ok.setStyleSheet("background-color: rgb(255, 255, 255);")
-        self.button_ok.setObjectName("button_ok")
-        self.button_cancel = QtWidgets.QPushButton(self.centralwidget)
-        self.button_cancel.setGeometry(QtCore.QRect(470, 342, 80, 30))
-        font = QtGui.QFont()
-        font.setFamily("Times New Roman")
-        font.setPointSize(14)
-        font.setBold(True)
-        font.setWeight(75)
-        self.button_cancel.setFont(font)
-        self.button_cancel.setStyleSheet("background-color: rgb(255, 255, 255);")
-        self.button_cancel.setObjectName("button_cancel")
         self.button_start = QtWidgets.QPushButton(self.centralwidget)
-        self.button_start.setGeometry(QtCore.QRect(250, 432, 271, 61))
+        self.button_start.setGeometry(QtCore.QRect(250, 430, 270, 60))
         font = QtGui.QFont()
         font.setFamily("Times New Roman")
         font.setPointSize(16)
         font.setBold(True)
         font.setWeight(75)
         self.button_start.setFont(font)
-        self.button_start.setStyleSheet("background-color: rgb(204, 109, 179);")
+        self.button_start.setStyleSheet("background-color: #66FBFF;\n"
+"border: 1px solid #AB1E24;\n"
+"")
         self.button_start.setObjectName("button_start")
         self.wallet = QtWidgets.QLabel(self.centralwidget)
-        self.wallet.setGeometry(QtCore.QRect(90, 140, 280, 30))
+        self.wallet.setGeometry(QtCore.QRect(90, 160, 610, 30))
         font = QtGui.QFont()
         font.setFamily("Times New Roman")
         font.setPointSize(24)
         font.setBold(True)
-        font.setUnderline(True)
+        font.setUnderline(False)
         font.setWeight(75)
         self.wallet.setFont(font)
-        self.wallet.setStyleSheet("color: rgb(255, 255, 255);")
+        self.wallet.setStyleSheet("background: rgba(0, 0, 0, 0);\n"
+"color: #D7D7D7;")
         self.wallet.setObjectName("wallet")
         self.pool = QtWidgets.QLabel(self.centralwidget)
-        self.pool.setGeometry(QtCore.QRect(90, 230, 280, 30))
+        self.pool.setGeometry(QtCore.QRect(90, 240, 610, 30))
         font = QtGui.QFont()
         font.setFamily("Times New Roman")
         font.setPointSize(24)
         font.setBold(True)
-        font.setUnderline(True)
+        font.setUnderline(False)
         font.setWeight(75)
         self.pool.setFont(font)
-        self.pool.setStyleSheet("color: rgb(255, 255, 255);")
+        self.pool.setStyleSheet("background: rgba(0, 0, 0, 0);\n"
+"color: #D7D7D7;")
         self.pool.setObjectName("pool")
         self.text_wallet = QtWidgets.QTextEdit(self.centralwidget)
-        self.text_wallet.setGeometry(QtCore.QRect(90, 190, 611, 31))
-        self.text_wallet.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.text_wallet.setGeometry(QtCore.QRect(90, 200, 610, 30))
+        self.text_wallet.setStyleSheet("background-color: #D7D7D7;")
         self.text_wallet.setObjectName("text_wallet")
         self.text_wallet_2 = QtWidgets.QTextEdit(self.centralwidget)
-        self.text_wallet_2.setGeometry(QtCore.QRect(90, 280, 611, 31))
-        self.text_wallet_2.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.text_wallet_2.setGeometry(QtCore.QRect(90, 280, 320, 30))
+        self.text_wallet_2.setStyleSheet("background-color: #D7D7D7;")
         self.text_wallet_2.setObjectName("text_wallet_2")
+        self.cpu = QtWidgets.QTextEdit(self.centralwidget)
+        self.cpu.setGeometry(QtCore.QRect(90, 370, 160, 30))
+        self.cpu.setStyleSheet("background-color: #D7D7D7;\n"
+"")
+        self.cpu.setObjectName("cpu")
+        self.cpuload = QtWidgets.QLabel(self.centralwidget)
+        self.cpuload.setGeometry(QtCore.QRect(90, 330, 610, 30))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(24)
+        font.setBold(True)
+        font.setUnderline(False)
+        font.setWeight(75)
+        self.cpuload.setFont(font)
+        self.cpuload.setStyleSheet("background: rgba(0, 0, 0, 0);\n"
+"color: #D7D7D7;")
+        self.cpuload.setObjectName("cpuload")
         self.logo = QtWidgets.QLabel(self.centralwidget)
-        self.logo.setGeometry(QtCore.QRect(66, 30, 671, 91))
-        self.logo.setFrameShadow(QtWidgets.QFrame.Plain)
+        self.logo.setGeometry(QtCore.QRect(50, 20, 128, 128))
+        self.logo.setStyleSheet("background: rgba(0, 0, 0, 0);")
         self.logo.setText("")
-        self.logo.setPixmap(QtGui.QPixmap("resurs/img/logo.png"))
+        self.logo.setPixmap(QtGui.QPixmap("resurs/img/128x128.png"))
         self.logo.setObjectName("logo")
+        self.wallet_2 = QtWidgets.QLabel(self.centralwidget)
+        self.wallet_2.setGeometry(QtCore.QRect(200, 70, 550, 40))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(24)
+        font.setBold(True)
+        font.setUnderline(False)
+        font.setWeight(75)
+        self.wallet_2.setFont(font)
+        self.wallet_2.setStyleSheet("background: rgba(0, 0, 0, 0);\n"
+"color: #D7D7D7;")
+        self.wallet_2.setObjectName("wallet_2")
+        self.fon = QtWidgets.QLabel(self.centralwidget)
+        self.fon.setGeometry(QtCore.QRect(0, 0, 800, 800))
+        self.fon.setText("")
+        self.fon.setPixmap(QtGui.QPixmap("resurs/img/bch.jpg"))
+        self.fon.setObjectName("fon")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(10, 520, 251, 16))
+        self.label.setGeometry(QtCore.QRect(10, 500, 240, 15))
+        self.label.setStyleSheet("background: rgba(0, 0, 0, 0);\n"
+"color: #D7D7D7;")
         self.label.setObjectName("label")
+        self.fon.raise_()
+        self.button_start.raise_()
+        self.wallet.raise_()
+        self.pool.raise_()
+        self.text_wallet.raise_()
+        self.text_wallet_2.raise_()
+        self.cpu.raise_()
+        self.cpuload.raise_()
+        self.logo.raise_()
+        self.wallet_2.raise_()
+        self.label.raise_()
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -90,34 +129,46 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "EvoX Miner"))
-        self.button_ok.setText(_translate("MainWindow", "OK"))
-        self.button_ok.clicked.connect(self.ok)
-        self.button_cancel.setText(_translate("MainWindow", "Cancel"))
-        self.button_cancel.clicked.connect(self.cancel)
         self.button_start.setText(_translate("MainWindow", "Start EvoX Miner"))
         self.button_start.clicked.connect(self.start)
         self.wallet.setText(_translate("MainWindow", "Your Wallet:"))
         self.pool.setText(_translate("MainWindow", "Your Pool:"))
-        self.label.setText(_translate("MainWindow", "Specially for Evolution Network from cosmos"))
+        self.cpuload.setText(_translate("MainWindow", "Threads:"))
+        self.wallet_2.setText(_translate("MainWindow", "EvoX Xmrig GUI Easy Miner"))
+        self.label.setText(_translate("MainWindow", "Specially for evolution-network.org made Cosmos"))
+        self.text_wallet_2.setText(user[1])
+        self.text_wallet.setText(user[0])
+        self.cpu.setText(user[2])
+
 
     def start(self):
-        os.startfile(r'resurs\xmrig\xmrig.exe')
-
-    def cancel(self):
-        sys.exit()
-
-    def ok(self):
         wallet = self.text_wallet.toPlainText()
         pool = self.text_wallet_2.toPlainText()
-        settings_end = []
-        pools = {}
+        threads = int(self.cpu.toPlainText())
+        ithreads = []
+        if threads >= quantity_threads:
+            for i in range(quantity_threads):
+                ithreads.append(i)
+        else:
+            for i in range(threads):
+                ithreads.append(i)
         import json
         with open(r'resurs\xmrig\config.json', 'r+') as file:
             config = json.load(file)
             config['pools'][0]['url'] = pool
             config['pools'][0]['user'] = wallet
+            config['cpu']['rx/arq'] = ithreads
+
         with open(r'resurs\xmrig\config.json', 'w') as file:
             json.dump(config, file)
+        rec = wallet + "," + pool + "," + str(threads)
+        user = open(r'resurs\user.txt', 'w')
+        user.write(rec)
+
+        os.startfile(r'resurs\xmrig\xmrig.exe')
+        sys.exit()
+
+
 
 if __name__ == "__main__":
     import sys
@@ -126,5 +177,11 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
+    myappid = 'mycompany.myproduct.subproduct.version'
+    QtWin.setCurrentProcessExplicitAppUserModelID(myappid)
+    apps = QtWidgets.QApplication(sys.argv)
+    apps.setWindowIcon(QtGui.QIcon('resurs\img\icon.ico'))
+    window = QtWidgets.QWidget()
+    window.setWindowIcon(QtGui.QIcon('resurs\img\icon.ico'))
     MainWindow.show()
     sys.exit(app.exec_())
